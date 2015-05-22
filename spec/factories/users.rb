3378fa3596c3a528a86ec53e883b783ad8a4cf36
Tom Mullaney <tpmullan@gmail.com>
2015-05-22 16:51:12 -0500

    Added empty user views and removed fake index

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password "password"
+    password_confirmation "password"
+    confirmed_at Date.today
   end
 
 end
