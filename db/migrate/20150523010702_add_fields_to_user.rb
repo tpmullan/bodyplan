c39e2cc2a5c62fc3e5d3a9cd11a13f59e3d31664
Tom Mullaney <tpmullan@gmail.com>
2015-05-30 23:26:06 -0500

    Fixed asset css background image in prod

--- db/migrate/20150523010702_add_fields_to_user.rb
+++ db/migrate/20150523010702_add_fields_to_user.rb
@@ -4,7 +4,6 @@ class AddFieldsToUser < ActiveRecord::Migration
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :bio, :text
-    add_column :users, :profile_pic, :attachment
     add_column :users, :phone, :string
     add_column :users, :website, :string
     add_column :users, :sex, :string
@@ -13,5 +12,6 @@ class AddFieldsToUser < ActiveRecord::Migration
     add_column :users, :ideal_body_type, :string
     add_column :users, :fitness_level, :string
     add_column :users, :equipment_access, :string
+    add_attachment :users, :profile_pic
   end
 end
