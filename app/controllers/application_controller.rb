cdd834acaf229e55e8b64f8743ceb0ed53dac339
Tom Mullaney <tpmullan@gmail.com>
2015-05-30 23:46:07 -0500

    Updated login views in preparation for omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :first_name, :last_name, :password) }
   end
 
   rescue_from CanCan::AccessDenied do |exception|
