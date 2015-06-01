da1bced6d0e9e31c5e8e6488b6bfc8b28c6c744b
Tom Mullaney <tom@gruupmeet.com>
2015-06-01 11:38:43 -0500

    Fixed programs test case for show

--- spec/controllers/programs_controller_spec.rb
+++ spec/controllers/programs_controller_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ProgramsController, type: :controller do
+  
+  login_user
 
+  # This should return the minimal set of attributes required to create a valid
+  # User. As you add validations to User, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) {
+    { name: "example", 
+      trending: true, 
+      price: 34.43, 
+      rating: 3.3, 
+      difficulty: 'medium' } 
+
+  }
+
+  let(:invalid_attributes) {
+    { }
+  }
+  
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -11,7 +29,8 @@ RSpec.describe ProgramsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      program = Program.create(valid_attributes)
+      get :show, {:id => program.to_param} 
       expect(response).to have_http_status(:success)
     end
   end
