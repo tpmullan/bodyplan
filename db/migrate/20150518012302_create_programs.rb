79a45f8da116478b37ad35bbca5e8d154a7243bb
Tom Mullaney <tom@gruupmeet.com>
2015-06-01 00:05:07 -0500

    Tried changing the migration again

--- db/migrate/20150518012302_create_programs.rb
+++ db/migrate/20150518012302_create_programs.rb
@@ -1,7 +1,7 @@
 class CreatePrograms < ActiveRecord::Migration
   def change
     create_table :programs do |t|
-      t.references :trainer, index: true, foreign_key: true, references: :users
+      t.references :trainer, references: :users, index: true, foreign_key: true
       t.string :name
       t.decimal :rating
       t.attachment :overview_video
