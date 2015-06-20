806fbdd8da5c70a4e5075b032cde5d6284e5982c
markscoin <mortega@utexas.edu>
2015-06-20 03:42:36 -0500

    all programs overview

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20150609035718) do
   create_table "programs", force: :cascade do |t|
     t.integer  "trainer_id"
     t.string   "name"
-    t.decimal  "rating"
+    t.integer  "rating"
     t.string   "overview_video_file_name"
     t.string   "overview_video_content_type"
     t.integer  "overview_video_file_size"
@@ -55,14 +55,13 @@ ActiveRecord::Schema.define(version: 20150609035718) do
     t.string   "cover_photo_content_type"
     t.integer  "cover_photo_file_size"
     t.datetime "cover_photo_updated_at"
-    t.decimal  "price"
+    t.integer  "price"
     t.string   "difficulty"
     t.string   "equipment_required"
     t.integer  "session_intervals"
-    t.boolean  "trending",                    default: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.decimal  "star0"
+    t.boolean  "trending"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "star5"
     t.integer  "star4"
     t.integer  "star3"
@@ -100,47 +99,7 @@ ActiveRecord::Schema.define(version: 20150609035718) do
   add_index "subscriptions", ["program_id"], name: "index_subscriptions_on_program_id"
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                    default: "", null: false
-    t.string   "encrypted_password",       default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",            default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",          default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_type",                default: 2
-    t.string   "first_name"
-    t.string   "last_name"
-    t.text     "bio"
-    t.string   "phone"
-    t.string   "website"
-    t.string   "sex"
-    t.string   "goal"
-    t.integer  "ideal_session"
-    t.string   "ideal_body_type"
-    t.string   "fitness_level"
-    t.string   "equipment_access"
-    t.string   "profile_pic_file_name"
-    t.string   "profile_pic_content_type"
-    t.integer  "profile_pic_file_size"
-    t.datetime "profile_pic_updated_at"
-  end
-
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+# Could not dump table "users" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
